34e141c69d03b895ad811406efc64b41ca1e1c05
Dongwook Lee <i015005@gmail.com>
2012-05-02 00:53:05 +0900

    cruise-control 문제 initial commit

---
+++ src/main/scala/misc/codejam/sra/c/CruiseControl.scala
@@ -0,0 +1,21 @@
+package misc.codejam.sra.c
+
+object CruiseControl extends App {
+	
+	import scala.io.Source._
+
+//	val caseReader = new CruiseControlCaseReader("B-small-practice.in");
+//
+//	var	total:Int = caseReader.headLine.toInt		//테스트 케이스 총 수  
+//
+//	for ( caseNo <- 1 to total) {
+//	   val levelInfos:List[LevelInfo] = caseReader.readNext()
+//	   val analyzer = new CruiseAnalyzer(levelInfos);
+//	   val shortest = analyzer.findShortestAll2StarCount() match  {
+//	     case -1 => "Too Bad"  
+//	     case x@_ => x.toString
+//	   }
+//	   Console.println( "Case #%d: %s".format(caseNo, shortest))
+//	}
+//	caseReader.closeStream()
+}
